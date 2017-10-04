773d32a210d03f94c843586d12d91512f0099d1d
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-10-04 10:04:15 +0200

    Fix thunk printer (#3)
    Fix compilation for thunk-printer branch.

--- bignums_syntax.ml
+++ bignums_syntax.ml
@@ -87,9 +87,9 @@ exception Non_closed
 (* parses a *non-negative* integer (from bigint.ml) into an int31
    wraps modulo 2^31 *)
 let int31_of_pos_bigint ?loc n =
-  let ref_construct = CAst.make ?loc @@ GRef (int31_construct, None) in
-  let ref_0 = CAst.make ?loc @@ GRef (int31_0, None) in
-  let ref_1 = CAst.make ?loc @@ GRef (int31_1, None) in
+  let ref_construct = DAst.make ?loc @@ GRef (int31_construct, None) in
+  let ref_0 = DAst.make ?loc @@ GRef (int31_0, None) in
+  let ref_1 = DAst.make ?loc @@ GRef (int31_1, None) in
   let rec args counter n =
     if counter <= 0 then
       []
@@ -97,7 +97,7 @@ let int31_of_pos_bigint ?loc n =
       let (q,r) = div2_with_rest n in
 	(if r then ref_1 else ref_0)::(args (counter-1) q)
   in
-  CAst.make ?loc @@ GApp (ref_construct, List.rev (args 31 n))
+  DAst.make ?loc @@ GApp (ref_construct, List.rev (args 31 n))
 
 let error_negative ?loc =
   CErrors.user_err ?loc ~hdr:"interp_int31" (Pp.str "int31 are only non-negative numbers.")
@@ -110,19 +110,23 @@ let interp_int31 ?loc n =
 
 (* Pretty prints an int31 *)
 
+let is_gr c r = match DAst.get c with
+| GRef (ref, _) -> eq_gr ref r
+| _ -> false
+
 let bigint_of_int31 =
   let rec args_parsing args cur =
     match args with
       | [] -> cur
-      | { CAst.v = GRef (b,_) }::l when eq_gr b int31_0 -> args_parsing l (mult_2 cur)
-      | { CAst.v = GRef (b,_) }::l when eq_gr b int31_1 -> args_parsing l (add_1 (mult_2 cur))
+      | b::l when is_gr b int31_0 -> args_parsing l (mult_2 cur)
+      | b::l when is_gr b int31_1 -> args_parsing l (add_1 (mult_2 cur))
       | _ -> raise Non_closed
   in
-  function
-  | { CAst.v = GApp ({ CAst.v = GRef (c, _)}, args) } when eq_gr c int31_construct -> args_parsing args zero
+  fun c -> match DAst.get c with
+  | GApp (c, args) when is_gr c int31_construct -> args_parsing args zero
   | _ -> raise Non_closed
 
-let uninterp_int31 i =
+let uninterp_int31 (AnyGlobConstr i) =
   try
     Some (bigint_of_int31 i)
   with Non_closed ->
@@ -132,7 +136,7 @@ let uninterp_int31 i =
 let _ = Notation.declare_numeral_interpreter int31_scope
   (int31_path, int31_module)
   interp_int31
-  ([CAst.make @@ GRef (int31_construct, None)],
+  ([DAst.make @@ GRef (int31_construct, None)],
    uninterp_int31,
    true)
 
@@ -163,40 +167,40 @@ let height bi =
 
 (* n must be a non-negative integer (from bigint.ml) *)
 let word_of_pos_bigint ?loc hght n =
-  let ref_W0 = CAst.make ?loc @@ GRef (Lazy.force zn2z_W0, None) in
-  let ref_WW = CAst.make ?loc @@ GRef (Lazy.force zn2z_WW, None) in
+  let ref_W0 = DAst.make ?loc @@ GRef (Lazy.force zn2z_W0, None) in
+  let ref_WW = DAst.make ?loc @@ GRef (Lazy.force zn2z_WW, None) in
   let rec decomp hgt n =
     if hgt <= 0 then
       int31_of_pos_bigint ?loc n
     else if equal n zero then
-      CAst.make ?loc @@ GApp (ref_W0, [CAst.make ?loc @@ GHole (Evar_kinds.InternalHole, Misctypes.IntroAnonymous, None)])
+      DAst.make ?loc @@ GApp (ref_W0, [DAst.make ?loc @@ GHole (Evar_kinds.InternalHole, Misctypes.IntroAnonymous, None)])
     else
       let (h,l) = split_at hgt n in
-      CAst.make ?loc @@ GApp (ref_WW, [CAst.make ?loc @@ GHole (Evar_kinds.InternalHole, Misctypes.IntroAnonymous, None);
+      DAst.make ?loc @@ GApp (ref_WW, [DAst.make ?loc @@ GHole (Evar_kinds.InternalHole, Misctypes.IntroAnonymous, None);
 			   decomp (hgt-1) h;
 			   decomp (hgt-1) l])
   in
   decomp hght n
 
 let nat_of_int ?loc n =
-  let ref_O = CAst.make ?loc (GRef (Coqlib.glob_O, None)) in
-  let ref_S = CAst.make ?loc (GRef (Coqlib.glob_S, None)) in
+  let ref_O = DAst.make ?loc (GRef (Coqlib.glob_O, None)) in
+  let ref_S = DAst.make ?loc (GRef (Coqlib.glob_S, None)) in
   let rec mk_nat acc n =
     if Int.equal n 0 then acc
     else
-      mk_nat (CAst.make ?loc (GApp (ref_S, [acc]))) (pred n)
+      mk_nat (DAst.make ?loc (GApp (ref_S, [acc]))) (pred n)
   in
   mk_nat ref_O n
 
 let bigN_of_pos_bigint ?loc n =
   let h = height n in
-  let ref_constructor = CAst.make ?loc @@ GRef (bigN_constructor h, None) in
+  let ref_constructor = DAst.make ?loc @@ GRef (bigN_constructor h, None) in
   let word = word_of_pos_bigint ?loc h n in
   let args =
     if h < n_inlined then [word]
     else [Nat_syntax_plugin.Nat_syntax.nat_of_int ?loc (of_int (h-n_inlined));word]
   in
-  CAst.make ?loc @@ GApp (ref_constructor, args)
+  DAst.make ?loc @@ GApp (ref_constructor, args)
 
 let bigN_error_negative ?loc =
   CErrors.user_err ?loc ~hdr:"interp_bigN" (Pp.str "bigN are only non-negative numbers.")
@@ -212,18 +216,18 @@ let interp_bigN ?loc n =
 
 let bigint_of_word =
   let rec get_height rc =
-    match rc with
-    | { CAst.v = GApp ({ CAst.v = GRef(c,_)}, [_;lft;rght]) }
-         when eq_gr c (Lazy.force zn2z_WW) ->
+    match DAst.get rc with
+    | GApp (c, [_;lft;rght])
+         when is_gr c (Lazy.force zn2z_WW) ->
       1+max (get_height lft) (get_height rght)
     | _ -> 0
   in
   let rec transform hght rc =
-    match rc with
-    | { CAst.v = GApp ({ CAst.v = GRef(c,_)},_)}
-         when eq_gr c (Lazy.force zn2z_W0) -> zero
-    | { CAst.v = GApp ({ CAst.v = GRef(c,_)}, [_;lft;rght]) }
-         when eq_gr c (Lazy.force zn2z_WW) ->
+    match DAst.get rc with
+    | GApp (c,_)
+         when is_gr c (Lazy.force zn2z_W0) -> zero
+    | GApp (c, [_;lft;rght])
+         when is_gr c (Lazy.force zn2z_WW) ->
       let new_hght = hght-1 in
       add (mult (rank new_hght)
              (transform new_hght lft))
@@ -235,12 +239,12 @@ let bigint_of_word =
     transform hght rc
 
 let bigint_of_bigN rc =
-  match rc with
-  | { CAst.v = GApp (_,[one_arg]) } -> bigint_of_word one_arg
-  | { CAst.v = GApp (_,[_;second_arg]) } -> bigint_of_word second_arg
+  match DAst.get rc with
+  | GApp (_,[one_arg]) -> bigint_of_word one_arg
+  | GApp (_,[_;second_arg]) -> bigint_of_word second_arg
   | _ -> raise Non_closed
 
-let uninterp_bigN rc =
+let uninterp_bigN (AnyGlobConstr rc) =
   try
     Some (bigint_of_bigN rc)
   with Non_closed ->
@@ -253,7 +257,7 @@ let uninterp_bigN rc =
 let bigN_list_of_constructors =
   let rec build i =
     if i < n_inlined+1 then
-      (CAst.make @@ GRef (bigN_constructor i,None))::(build (i+1))
+      (DAst.make @@ GRef (bigN_constructor i,None))::(build (i+1))
     else
       []
   in
@@ -270,17 +274,17 @@ let _ = Notation.declare_numeral_interpreter bigN_scope
 
 (*** Parsing for bigZ in digital notation ***)
 let interp_bigZ ?loc n =
-  let ref_pos = CAst.make ?loc @@ GRef (bigZ_pos, None) in
-  let ref_neg = CAst.make ?loc @@ GRef (bigZ_neg, None) in
+  let ref_pos = DAst.make ?loc @@ GRef (bigZ_pos, None) in
+  let ref_neg = DAst.make ?loc @@ GRef (bigZ_neg, None) in
   if is_pos_or_zero n then
-    CAst.make ?loc @@ GApp (ref_pos, [bigN_of_pos_bigint ?loc n])
+    DAst.make ?loc @@ GApp (ref_pos, [bigN_of_pos_bigint ?loc n])
   else
-    CAst.make ?loc @@ GApp (ref_neg, [bigN_of_pos_bigint ?loc (neg n)])
+    DAst.make ?loc @@ GApp (ref_neg, [bigN_of_pos_bigint ?loc (neg n)])
 
 (* pretty printing functions for bigZ *)
-let bigint_of_bigZ = function
-  | { CAst.v = GApp ({ CAst.v = GRef(c,_) }, [one_arg])} when eq_gr c bigZ_pos -> bigint_of_bigN one_arg
-  | { CAst.v = GApp ({ CAst.v = GRef(c,_) }, [one_arg])} when eq_gr c bigZ_neg ->
+let bigint_of_bigZ c = match DAst.get c with
+  | GApp (c, [one_arg]) when is_gr c bigZ_pos -> bigint_of_bigN one_arg
+  | GApp (c, [one_arg]) when is_gr c bigZ_neg ->
       let opp_val = bigint_of_bigN one_arg in
       if equal opp_val zero then
 	raise Non_closed
@@ -289,7 +293,7 @@ let bigint_of_bigZ = function
   | _ -> raise Non_closed
 
 
-let uninterp_bigZ rc =
+let uninterp_bigZ (AnyGlobConstr rc) =
   try
     Some (bigint_of_bigZ rc)
   with Non_closed ->
@@ -299,19 +303,19 @@ let uninterp_bigZ rc =
 let _ = Notation.declare_numeral_interpreter bigZ_scope
   (bigZ_path, bigZ_module)
   interp_bigZ
-  ([CAst.make @@ GRef (bigZ_pos, None);
-    CAst.make @@ GRef (bigZ_neg, None)],
+  ([DAst.make @@ GRef (bigZ_pos, None);
+    DAst.make @@ GRef (bigZ_neg, None)],
    uninterp_bigZ,
    true)
 
 (*** Parsing for bigQ in digital notation ***)
 let interp_bigQ ?loc n =
-  let ref_z = CAst.make ?loc @@ GRef (bigQ_z, None) in
-  CAst.make ?loc @@ GApp (ref_z, [interp_bigZ ?loc n])
+  let ref_z = DAst.make ?loc @@ GRef (bigQ_z, None) in
+  DAst.make ?loc @@ GApp (ref_z, [interp_bigZ ?loc n])
 
-let uninterp_bigQ rc =
-  try match rc with
-    | { CAst.v = GApp ({ CAst.v = GRef(c,_)}, [one_arg]) } when eq_gr c bigQ_z ->
+let uninterp_bigQ (AnyGlobConstr rc) =
+  try match DAst.get rc with
+    | GApp (c, [one_arg]) when is_gr c bigQ_z ->
 	Some (bigint_of_bigZ one_arg)
     | _ -> None (* we don't pretty-print yet fractions *)
   with Non_closed -> None
@@ -320,5 +324,5 @@ let uninterp_bigQ rc =
 let _ = Notation.declare_numeral_interpreter bigQ_scope
   (bigQ_path, bigQ_module)
   interp_bigQ
-  ([CAst.make @@ GRef (bigQ_z, None)], uninterp_bigQ,
+  ([DAst.make @@ GRef (bigQ_z, None)], uninterp_bigQ,
    true)
