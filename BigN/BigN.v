cb914338a82409e94cb0ff18ec48d95a6a2113c5
Pierre Roux <pierre.roux@onera.fr>
2023-03-14 16:40:39 +0100

    Remove the [ _ ] notations
    They weren't extensively used and conflicted with too many things like
    primitive arrays or list notations. For instance, the following code
    was failing:
    
    ```Coq
    Require Import PArray Uint63.
    
    Check [|true; true | false|].
    
    From Bignums Require Import BigN.
    
    Fail Check [|true; true | false|].
    (* Error: Syntax error: [term level 200] expected after '[' (in [term]). *)
    ```

--- BigN/BigN.v
+++ BigN/BigN.v
@@ -67,7 +67,6 @@ Notation "x < y < z" := (x<y /\ y<z) : bigN_scope.
 Notation "x < y <= z" := (x<y /\ y<=z) : bigN_scope.
 Notation "x <= y < z" := (x<=y /\ y<z) : bigN_scope.
 Notation "x <= y <= z" := (x<=y /\ y<=z) : bigN_scope.
-Notation "[ i ]" := (BigN.to_Z i) : bigN_scope.
 Infix "mod" := BigN.modulo (at level 40, no associativity) : bigN_scope.
 
 (** Example of reasoning about [BigN] *)
@@ -110,7 +109,7 @@ case Z.eqb_spec.
 BigN.zify. auto with zarith.
 intros NEQ.
 generalize (BigN.spec_div_eucl a b).
-generalize (Z_div_mod_full [a] [b] NEQ).
+generalize (Z_div_mod_full (BigN.to_Z a) (BigN.to_Z b) NEQ).
 destruct BigN.div_eucl as (q,r), Z.div_eucl as (q',r').
 intros (EQ,_). injection 1 as EQr EQq.
 BigN.zify. rewrite EQr, EQq; auto.
